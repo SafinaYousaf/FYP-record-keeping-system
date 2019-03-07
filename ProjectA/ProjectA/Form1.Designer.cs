0546caf5d1c1a888c4628b17231dd2c59d9f7e83
SafinaYousaf <41517325+SafinaYousaf@users.noreply.github.com>
2019-03-07 10:38:00 +0500

    ...

---
+++ ProjectA/ProjectA/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ProjectA
+{
+    partial class Display
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StudentGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentGrid
+            // 
+            this.StudentGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StudentGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.StudentGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentGrid.Location = new System.Drawing.Point(143, 37);
+            this.StudentGrid.Name = "StudentGrid";
+            this.StudentGrid.Size = new System.Drawing.Size(614, 256);
+            this.StudentGrid.TabIndex = 0;
+            // 
+            // Display
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StudentGrid);
+            this.Name = "Display";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView StudentGrid;
+    }
+}
+
---
+++ ProjectA/ProjectA/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectA
+{
+    public partial class Display : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=SAFINAYOUSAF\SAFINA;Initial Catalog=ProjectA;Integrated Security=True");
+        public Display()
+        {
+            InitializeComponent();
+        }
+        public void disp_data()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Student, Person";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            StudentGrid.DataSource = dt; //Student is name of data grid view present on form
+            con.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+    }
+}
